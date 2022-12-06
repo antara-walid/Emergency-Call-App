181831792ea86fae67e501c8adfe36ed1638b3be
antara-walid <antara.walid.27@gmail.com>
2022-12-06 09:20:55 +0100

    feat(MainActivity): add an other message for testing purposes

--- app/src/main/java/com/example/emergencycallapp/MainActivity.java
+++ app/src/main/java/com/example/emergencycallapp/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // testing git
+        // testing git dev branch
     }
 }
