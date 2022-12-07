768fcc1004ce9ff8fe9ced73db4adb6c7308f1b3
antara-walid <antara.walid.27@gmail.com>
2022-12-07 17:32:21 +0100

    add call phone permission

--- app/src/main/java/com/example/emergencycallapp/EmergencyListActivity.java
+++ app/src/main/java/com/example/emergencycallapp/EmergencyListActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class EmergencyListActivity extends AppCompatActivity {
 
@@ -18,6 +20,25 @@ public class EmergencyListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.emergency_list);
+
+        // selecting cards
+
+        cardViewFire = findViewById(R.id.cardViewFire);
+        cardViewFlood= findViewById(R.id.cardViewFlood);
+        cardViewCrime = findViewById(R.id.cardViewCrime);
+        cardViewAccident = findViewById(R.id.cardViewAccident);
+        cardViewEarthQuake = findViewById(R.id.cardViewEarthQuake);
+        cardViewIllness = findViewById(R.id.cardViewIllness);
+
+        // listeners
+
+        cardViewFire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(EmergencyListActivity.this,"you clicked fire" ,Toast.LENGTH_SHORT ).show();
+            }
+        });
+
 
     }
 }
