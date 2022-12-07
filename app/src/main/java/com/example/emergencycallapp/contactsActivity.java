77fbd4f772b4f7c26fe1c6d83b34e3f299bed6df
Hamza AitBamoh <hamzaaitbamoh@gmail.com>
2022-12-07 23:10:36 +0100

    added create activity

---
+++ app/src/main/java/com/example/emergencycallapp/contactsActivity.java
@@ -0,0 +1,15 @@
+package com.example.emergencycallapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class contactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+    }
+}
