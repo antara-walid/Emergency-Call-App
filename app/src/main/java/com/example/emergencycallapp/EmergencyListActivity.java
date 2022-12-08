93abc332cddd5b9093d8cbdd0e99283c7c0ce71d
antara-walid <antara.walid.27@gmail.com>
2022-12-08 11:27:45 +0100

    get location

--- app/src/main/java/com/example/emergencycallapp/EmergencyListActivity.java
+++ app/src/main/java/com/example/emergencycallapp/EmergencyListActivity.java
@@ -9,12 +9,20 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmergencyListActivity extends AppCompatActivity {
 
@@ -26,15 +34,52 @@ public class EmergencyListActivity extends AppCompatActivity {
     private CardView cardViewEarthQuake;
     private CardView cardViewIllness;
     private ImageView imageViewBackIcon;
+    private TextView textViewMessage;
     private static String number;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    String[] appPermissions = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.SEND_SMS
+    };
+
+    private static final int PERMISSIONS_REQUEST_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.emergency_list);
 
-        // asking for permission
-        ActivityCompat.requestPermissions(EmergencyListActivity.this, new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
+        textViewMessage = findViewById(R.id.textViewEmergencyMessage); // delete it after
+        // asking for permissions
+        if (checkRequestPermissions()) {
+
+        }
+
+        // location manager
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.d("location", location.toString());
+                //textViewMessage.setText(location.getLatitude() + " - "+ location.getLongitude());
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
 
         // selecting cards
 
@@ -126,6 +171,23 @@ public class EmergencyListActivity extends AppCompatActivity {
 
     }
 
+    private boolean checkRequestPermissions() {
+        List<String> listOfPermissionsNeeded = new ArrayList<>();
+        for (String permission : appPermissions) {
+            if (ContextCompat.checkSelfPermission(EmergencyListActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+                listOfPermissionsNeeded.add(permission); // so that we can ask for them later
+            }
+        }
+
+        if(!listOfPermissionsNeeded.isEmpty())
+        {
+            ActivityCompat.requestPermissions(EmergencyListActivity.this,listOfPermissionsNeeded.toArray(
+                    new String[listOfPermissionsNeeded.size()]) ,PERMISSIONS_REQUEST_CODE);
+            return false;
+        }
+        return true;
+    }
+
     private void makePhoneCall(String number) {
         if (ContextCompat.checkSelfPermission(EmergencyListActivity.this,
                 Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
@@ -138,7 +200,7 @@ public class EmergencyListActivity extends AppCompatActivity {
     }
 
     private void sendSms(String sms) {
-        ActivityCompat.requestPermissions(EmergencyListActivity.this, new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
+      //  ActivityCompat.requestPermissions(EmergencyListActivity.this, new String[]{Manifest.permission.SEND_SMS}, PackageManager.PERMISSION_GRANTED);
         SmsManager smsManager = SmsManager.getDefault();
         smsManager.sendTextMessage(number, null, sms, null, null);
     }
