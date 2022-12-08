daedc480fe4d8cf6b6d6c39316af8d31d8c722f5
Hamza AitBamoh <hamzaaitbamoh@gmail.com>
2022-12-08 11:47:00 +0100

    ability to surf between contactActivity and landingActivity

--- app/src/main/java/com/example/emergencycallapp/landingActivity.java
+++ app/src/main/java/com/example/emergencycallapp/landingActivity.java
@@ -2,14 +2,36 @@ package com.example.emergencycallapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class landingActivity extends AppCompatActivity {
-
+    Button sosbtn;
+    ImageView add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-
+        sosbtn=findViewById(R.id.sosbtn);
+        add=findViewById(R.id.imageViewBackIcon);
+        sosbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent=new Intent(landingActivity.this,EmergencyListActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent=new Intent(landingActivity.this,contactsActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        });
     }
 }
