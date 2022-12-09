9258057bfafd0910bf1840753e7bb4657a73e9dc
antara-walid <antara.walid.27@gmail.com>
2022-12-09 19:37:00 +0100

    create custom listAdapter

---
+++ app/src/main/java/com/example/emergencycallapp/ListAdapter.java
@@ -0,0 +1,42 @@
+package com.example.emergencycallapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends ArrayAdapter<EmergencyContact> {
+
+    public ListAdapter(Context context , ArrayList<EmergencyContact> list)
+    {
+        super(context,R.layout.list_item,list);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        EmergencyContact contact = getItem(position);
+
+        if(convertView == null)
+        {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+        }
+
+        TextView fullName = convertView.findViewById(R.id.fullName);
+        TextView phoneNumber = convertView.findViewById(R.id.phoneNumber);
+
+        fullName.setText(contact.getFullName());
+        phoneNumber.setText(contact.getPhoneNumber());
+
+        return super.getView(position, convertView, parent);
+    }
+}
