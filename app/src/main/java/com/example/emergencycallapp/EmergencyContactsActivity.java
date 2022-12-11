b17e2265fee5c5df9fde1afe6cf5cef46b8d16ff
antara-walid <antara.walid.27@gmail.com>
2022-12-11 19:47:04 +0100

    use database instead of static data

--- app/src/main/java/com/example/emergencycallapp/EmergencyContactsActivity.java
+++ app/src/main/java/com/example/emergencycallapp/EmergencyContactsActivity.java
@@ -3,6 +3,7 @@ package com.example.emergencycallapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -13,10 +14,13 @@ import android.widget.Toast;
 import com.example.emergencycallapp.databinding.ActivityEmergencyContactsBinding;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EmergencyContactsActivity extends AppCompatActivity {
 
     ActivityEmergencyContactsBinding binding;
+
+
 
     private ImageView imageViewBackIcon;
     @Override
@@ -39,16 +43,36 @@ public class EmergencyContactsActivity extends AppCompatActivity {
         });
 
 
-        // static data for test
+        // database
+        MyDatabaseHelper myDatabaseHelper = new MyDatabaseHelper(EmergencyContactsActivity.this);
+        //List<EmergencyContact> emergencyContactList = myDatabaseHelper.getAll();
+        List<EmergencyContact> emergencyContactList = new ArrayList<>();
+        Cursor cursor = myDatabaseHelper.readAllData();
+        if(cursor.getCount() != 0)
+        {
+            while (cursor.moveToNext())
+            {
+                EmergencyContact contact = new EmergencyContact(cursor.getString(1),cursor.getString(2));
+                emergencyContactList.add(contact);
+            }
+        }
 
-        String [] fullNames = {"walid 404" ,"hamza slow"};
-        String [] phoneNumbers = {"06171238" ,"06178382"};
+       // emergencyContactList.add(new EmergencyContact("test" ,"0000000"));
+        System.out.println( "list eme : " +emergencyContactList);
+
+        List<String> fullNames  = new ArrayList<>();
+        List<String> phoneNumbers = new ArrayList<>();
+        for(EmergencyContact contact : emergencyContactList)
+        {
+            fullNames.add(contact.getFullName());
+            phoneNumbers.add(contact.getPhoneNumber());
+        }
 
         ArrayList<EmergencyContact> contactsList = new ArrayList<>();
 
-        for(int i = 0 ; i <fullNames.length ;i++)
+        for(int i = 0 ; i <fullNames.size() ;i++)
         {
-            EmergencyContact contact = new EmergencyContact(fullNames[i],phoneNumbers[i] );
+            EmergencyContact contact = new EmergencyContact(fullNames.get(i),phoneNumbers.get(i) );
             contactsList.add(contact);
         }
 
@@ -59,8 +83,8 @@ public class EmergencyContactsActivity extends AppCompatActivity {
         binding.listViewContacts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String text = fullNames[i];
-                String phoneNumber = phoneNumbers[i];
+                String text = fullNames.get(i);
+                String phoneNumber = phoneNumbers.get(i);
                 makePhoneCall(phoneNumber);
             }
         });
